59b0dcc95009197fe415b9b5a5859dc0e207ac72
jbluemeninj@hotmail.com <>
2024-10-23 11:49:01 -0500

    fitst commit

---
+++ src/test/java/com/jbluemeninj/JavaBackendDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbluemeninj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
